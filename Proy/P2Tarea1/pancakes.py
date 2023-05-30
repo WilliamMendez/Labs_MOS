c564e643972f5e2acd06f85649e3c771d94bf250
WilliamMendez <69637352+WilliamMendez@users.noreply.github.com>
2023-05-30 00:28:10 -0500

    Update pancakes.py

--- Proy/P2Tarea1/pancakes.py
+++ Proy/P2Tarea1/pancakes.py
@@ -1,42 +1,51 @@
-"""
-AUTORES:
-Luis Felipe Torres
-Daniel Felipe Vargas
-"""
-def flip(arr, k):
-    return arr[:k] + arr[k:][::-1]
+
+def flip(s, k):
+    if k == 1:
+        return s
+    else:
+        return s[:k][::-1] + s[k:]
 
 def pancakes(arr):
+    print(arr)
+    print("\n")
     flips = []
     n = len(arr)
-    l = n
     while n > 0:
-
         max_index = arr.index(n)
-        if max_index != 0:
-            if max_index!=len(arr)-1:
-                arr = flip(arr, max_index)
-                flips.append(max_index)
-            arr = flip(arr, len(arr)-n)
-            flips.append(len(arr)-n)
+        if max_index != n-1: # si no está en su lugar toca moverlo
+            if max_index!=0: # si no está en la primera posición toca ponerlo en la primera para luego ponerlo en la última
+                print("flip en", max_index+1)
+                flips.append(max_index+1)
+                arr = flip(arr, max_index+1)
+                print(arr)
+            print("flip en", n, "para acomodar")
+            flips.append(n)
+            arr = flip(arr, n) # se pone en la última posición
+            print(arr)
+            print("\n")
         n -= 1
 
-        for i in range(n, 0, -1):
-            if arr[l-i] == n:
+        for i in range(n, 0, -1): # se buscan los siguientes elementos para saber si están ordenados
+            if arr[n-1] == n:
                 n-=1
             else:
                 break
+
     return flips
 
+
 def main():
-    n = int(input())
-    for j in range(n):
-        arr = [int(i) for i in input().split()]
-        flips = pancakes(arr)
-        if flips:
-            flips = [str(x) for x in flips]
-            print(" ".join(flips))
-        else:
-            print("ORDENADO")
+    arr = [int(i) for i in list(input("Ingrese el estado inicial: "))]
+    flips = pancakes(arr)
+    if flips:
+        flips = [str(x) for x in flips]
+        print(" ".join(flips))
+        print("# de flips en cada posición: ")
+        for i in range(1, len(arr)+1):
+            print(i, ":", flips.count(str(i)))
+
+    else:
+        print("ORDENADO")
+
 
 main()
