432c010ec8e8aa4a00c1f4cc321c7cc9c71738e9
WilliamMendez <69637352+WilliamMendez@users.noreply.github.com>
2023-05-31 01:27:17 -0500

    El puto parcial

--- Parcial3/punto3.m
+++ Parcial3/punto3.m
@@ -50,39 +50,94 @@ end
 
 %% SIMULADOR DE EVENTOS DISCRETOS
 % Inicializacion de variables
+inicio = 9;
+final = 17;
+visitados = containers.Map('KeyType','int32','ValueType','int32'); % Almacena el nodo por el que se llega más rápido al nodo llave
+visitados(inicio) = inicio;
+completo = 0;
 
 % Inicialización del tiempo de simulación
 t=0;
 
 % Programacion del evento inicial o los eventos iniciales.
-
 % Adiciono el evento inicial o los eventos iniciales a la cola de eventos.
-
+evtQueue = [];
+for i=1:length(nodesCoordinates)
+    if matrLinks(inicio, i) < inf
+        newEvt.t = matrLinks(inicio,i)*3.333333333;
+        newEvt.type = 'A';
+        newEvt.origen = inicio;
+        newEvt.destino = i;
+        evtQueue = [evtQueue newEvt];
+    end
+end
+% Organizacion de la cola de eventos
+flag=1;
+while flag==1
+    flag=0; 
+    for i=1:(length(evtQueue)-1)
+        if evtQueue(i).t>evtQueue(i+1).t
+            temp=evtQueue(i);
+            evtQueue(i)=evtQueue(i+1);
+            evtQueue(i+1)=temp;
+            flag=1; 
+        end
+    end
+end
+%Mostrado de la cola de eventos:
+fprintf('\nCola de eventos:\n');
+for i=1:length(evtQueue)
+    fprintf('Evento %s en t=%f de %d a %d\n',evtQueue(i).type,evtQueue(i).t, evtQueue(i).origen, evtQueue(i).destino);
+end
+fprintf('----------------\n');
+%pause(5);
 
 % Desarrollo de la simulacion
-while length(evtQueue)>0
+while ~isempty(evtQueue) && completo == 0
     evtAct=evtQueue(1); 
     evtQueue(1)=[];
     
     t=evtAct.t;
+    ori = evtAct.origen;
+    des = evtAct.destino;
     
     % Procesamiento del evento A
-    if evtAct.type=='A'
-        
-                
+    if evtAct.type=='A' && ~isKey(visitados, des)
+        if inicio == final
+            completo = 1;
+        else        
+            fprintf('Entra un evento A\n')
+            evtAct.type='B';
+            visitados(des) = ori;  
+            plot(nodesCoordinates(des,1), nodesCoordinates(des,2), 'o', 'LineWidth',1,'MarkerEdgeColor','b', 'MarkerFaceColor','b', 'MarkerSize',7);
+            fprintf('Se llego desde %d a %d\n', ori, des)
+            %evtQueue = [evtQueue evtAct];
+            if des == final
+                completo = 1;
+            end
+        end
     end 
     
     % Procesamiento del evento B
     if evtAct.type=='B'
-        
-                
-    end   
-    
-    % Procesamiento del evento Z
-    if evtAct.type=='Z'
-        
-                
-    end   
+        fprintf('Entra un evento B\n')
+        for i=1:30 
+            dij = matrLinks(des, i);
+            if dij<inf && isKey(visitados, i) == 0 && i~=ori
+                % crear evento
+                newEvt.t = t + (dij*3.3333);
+                newEvt.origen = des;
+                newEvt.destino = i;
+                newEvt.type = 'A';
+                evtQueue = [evtQueue newEvt];
+                fprintf('Se agrega %d a %d\n', des, i);
+                %pause(0.2)
+            end
+        end     
+    end
+
+    fprintf('T=%f\n',t);
+
        
     % Organizacion de la cola de eventos
     flag=1;
@@ -100,10 +155,25 @@ while length(evtQueue)>0
     % Mostrado de la cola de eventos:
     fprintf('\nCola de eventos:\n');
     for i=1:length(evtQueue)
-        fprintf('Evento %s en t=%f\n',evtQueue(i).type,evtQueue(i).t);
+        fprintf('Evento %s en t=%f de %d a %d\n',evtQueue(i).type,evtQueue(i).t, evtQueue(i).origen, evtQueue(i).destino);
     end
     fprintf('----------------\n');
     % pause;
 end
 
-fprintf('Tiempo transcurrido: %f  \n',t);
+% Pintar la ruta tomada de rojo
+completo = 0;
+% pintar de rojo el final
+plot(nodesCoordinates(final,1), nodesCoordinates(final,2), 'o', 'LineWidth',1,'MarkerEdgeColor','r', 'MarkerFaceColor','r', 'MarkerSize',7);
+actual = visitados(final);
+while completo ~= 1
+    %fprintf('%d',actual);
+    if actual == inicio
+        completo =1;
+    end
+    plot(nodesCoordinates(actual,1), nodesCoordinates(actual,2), 'o', 'LineWidth',1,'MarkerEdgeColor','r', 'MarkerFaceColor','r', 'MarkerSize',7);
+    actual = visitados(actual);
+    %pause(0.5)
+end
+
+fprintf('Tiempo transcurrido para llegar a %d : %f  \n',final, t);
